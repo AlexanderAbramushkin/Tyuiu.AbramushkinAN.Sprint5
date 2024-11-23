088713a2497b187a65dc56f2ce22e93476695bd3
Tyuiu.AbramushkinAN@outlook.com <Tyuiu.AbramushkinAN@outlook.com>
2024-11-23 17:14:09 +0500

    Task4

--- Tyuiu.AbramushkinAN.Sprint5.Task4.V28.Lib/DataService.cs
+++ Tyuiu.AbramushkinAN.Sprint5.Task4.V28.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.AbramushkinAN.Sprint5.Task4.V28.Lib
 {
@@ -7,7 +8,7 @@ namespace Tyuiu.AbramushkinAN.Sprint5.Task4.V28.Lib
         public double LoadFromDataFile(string path)
         {
             string strx = File.ReadAllText(path);
-            double x = Double.Parse(strx);
+            double x = Double.Parse(strx, CultureInfo.InvariantCulture);
             double res = Math.Round((3 * Math.Pow(x, 3)) / (Math.Sin(x)),3);
             return res;
         }
